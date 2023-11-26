f8c7469638ac5d44f8e7a3b9495862fb93aa0bf4
fthearslan <146673937+fthearslan@users.noreply.github.com>
2023-11-26 13:40:26 +0100

    Add project files.

---
+++ GonulInsanlari/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GonulInsanlari.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
