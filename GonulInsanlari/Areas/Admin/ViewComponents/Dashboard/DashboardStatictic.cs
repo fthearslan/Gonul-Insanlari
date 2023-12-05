5f0996c4cf2ff2fa22ad963ebd34a9d888d01fcf
fthearslan <146673937+fthearslan@users.noreply.github.com>
2023-12-05 17:45:06 +0100

    Adjusments on Messageboxes

---
+++ GonulInsanlari/Areas/Admin/ViewComponents/Dashboard/DashboardStatictic.cs
@@ -0,0 +1,24 @@
+﻿using BussinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GonulInsanlari.Areas.Admin.ViewComponents.Dashboard
+{
+    public class DashboardStatictic:ViewComponent
+    {
+        CommentManager commentManager = new CommentManager(new EFCommentDAL());
+        ArticleManager articleManager= new ArticleManager(new EFArticleDAL());
+        VideoManager videoManager= new VideoManager(new EFVideoDAL());
+        public IViewComponentResult Invoke()
+        {
+            var commentcount = commentManager.List().Count;
+            ViewBag.CommentCount = commentcount;
+            var articlecount = articleManager.ListFilter().Count();
+            ViewBag.ArticleCount = articlecount;
+            var videocount = videoManager.ListFilter().Count();
+            ViewBag.VideoCount = videocount;
+            return View();
+        }
+    }
+}
