7433b480e3e3c001ba78a430d0f6ea307185cc44
fthearslan <146673937+fthearslan@users.noreply.github.com>
2024-11-10 00:11:12 +0100

    [feature]:Announcement has been retrieved on user-side.

---
+++ GonulInsanlari/ViewComponents/SideBarAnnouncementComponent.cs
@@ -0,0 +1,35 @@
+﻿using BussinessLayer.Abstract.Services;
+using Microsoft.AspNetCore.Mvc;
+using ViewModelLayer.ViewModels.Announcement;
+
+namespace GonulInsanlari.ViewComponents
+{
+    public class SideBarAnnouncementComponent : ViewComponent
+    {
+        IAnnouncementService _manager;
+
+        public SideBarAnnouncementComponent(IAnnouncementService manager)
+        {
+            _manager = manager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            List<AnnouncementSideBarViewModel> models = _manager.GetWhere(x => x.Status == true && x.IsForAdmins == false)
+                .OrderByDescending(x => x.IsAttached == true)
+                .Select(x => new AnnouncementSideBarViewModel()
+                {
+                    ID = x.Id,
+                    Title = x.Title,
+                    Created = x.Created,
+
+                }).ToList();
+
+            return View(models);
+
+
+        }
+
+
+    }
+}
---
+++ ModelLayer/ViewModels/Announcement/AnnouncementSideBarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModelLayer.ViewModels.Announcement
+{
+    public class AnnouncementSideBarViewModel
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
